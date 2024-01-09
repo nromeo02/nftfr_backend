3b74db76a146c2d99eb867c538081e7443986db5
fvesc1 <vscfnc02e24m208p@studenti.unical.it>
2024-01-09 23:34:55 +0100

    Acciderbolina che divertimento

--- src/main/java/org/nftfr/backend/persistence/dao/postgres/NftDaoPostgres.java
+++ src/main/java/org/nftfr/backend/persistence/dao/postgres/NftDaoPostgres.java
@@ -1,4 +1,51 @@
 package org.nftfr.backend.persistence.dao.postgres;
 
-public class NftDaoPostgres {
+import org.nftfr.backend.persistence.dao.NftDao;
+import org.nftfr.backend.persistence.model.Nft;
+
+import java.sql.Connection;
+import java.util.List;
+
+public class NftDaoPostgres implements NftDao {
+    private final Connection connection;
+    public NftDaoPostgres(Connection conn){
+        this.connection = conn;
+    }
+    @Override
+    public Nft findByPrimaryKey(String id){
+        Nft nft = null;
+        String query = "select * from nft where id = ?";
+        return nft;
+
+    }
+
+    @Override
+    public void create(Nft nft) {
+
+    }
+
+    @Override
+    public void update(Nft nft) {
+
+    }
+
+    @Override
+    public void delete(Nft nft) {
+
+    }
+
+    @Override
+    public List<Nft> findByQuery(List<String> search) {
+        return null;
+    }
+
+    @Override
+    public List<Nft> findByTag(List<String> tag) {
+        return null;
+    }
+
+    @Override
+    public List<Nft> findByValue(int max, int min) {
+        return null;
+    }
 }
